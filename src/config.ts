e807adca29e8257de73feac0f88ff61ff566aae4
xinvxueyuan <xinvxueyuan@yeah.net>
2026-03-28 00:46:21 +0800

    更新站点URL为新的域名

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UT
 export const siteConfig: SiteConfig = {
 	title: "xinvStar",
 	subtitle: "新v学员个人博客",
-	siteURL: "https://xinvxueyuan.github.io/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://xinvstar.xyz/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-03-28", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	timeZone: SITE_TIMEZONE,
